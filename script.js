70d4ec67dfc3de0e410dce006b24dabb72d1d02e
buchacha <egor322129@gmail.com>
2020-09-20 09:42:45 +0000

    delete element from arr

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-alert(!undefined);
-alert(!0);
-alert(!'');
-alert(!null);
+let arr = [1,2,3,4,5];
+
+let handler = 5;
+
+let n = 4;
+alert(arr.splice(n--,1));
+
+alert(n);
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === handler) {
+        arr.splice(i--, 1);
+    }
+}
+
+alert(arr);
