c7180974b4f77c770fb252ab0d321e24dd330ad3
buchacha <egor322129@gmail.com>
2020-09-20 07:52:52 +0000

    extends

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function User(name) {
-  
-  this.name = name;
-  this.isAdmin = false;
+class Animal {
 
-  this.sayHi = function() { alert(this.name); };
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} бежит со скоростью ${this.speed}.`);
+  }
+
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} стоит.`);
+  }
+
 }
 
-let user = new User("Вася");
+class Rabbit extends Animal {
+  hide() {
+    alert(`${this.name} прячется!`);
+  }
 
-alert(user.name); // Вася
-alert(user.isAdmin); // false
-user.sayHi();
+  stop() {
+    super.stop(); // вызываем родительский метод stop
+    this.hide(); // и затем hide
+  }
+}
+
+let rabbit = new Rabbit("Белый кролик");
+
+rabbit.run(5); // Белый кролик бежит со скоростью 5.
+rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
