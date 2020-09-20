e9be28de3aa70202ea0d371449e9c17ffd30376b
buchacha <egor322129@gmail.com>
2020-09-20 08:11:05 +0000

    extends , options

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
-class Animal {
+let lowResolutionClock = new ExtendedClock({
+    template: 'h:m:s',
+    precision: 100
+  });
 
-  constructor(name) {
-    this.speed = 0;
-    this.name = name;
-  }
-
-  run(speed) {
-    this.speed = speed;
-    alert(`${this.name} бежит со скоростью ${this.speed}.`);
-  }
-
-  stop() {
-    this.speed = 0;
-    alert(`${this.name} стоит.`);
-  }
-
-}
-
-class Rabbit extends Animal {
-  hide() {
-    alert(`${this.name} прячется!`);
-  }
-
-  stop() {
-    super.stop(); // вызываем родительский метод stop
-    this.hide(); // и затем hide
-  }
-}
-
-let rabbit = new Rabbit("Белый кролик");
-
-rabbit.run(5); // Белый кролик бежит со скоростью 5.
-rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
+  lowResolutionClock.start();
