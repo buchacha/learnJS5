6d0d89906f51abef0b6895677e6c3a9f194fecbb
buchacha <egor322129@gmail.com>
2020-09-20 07:52:15 +0000

    functional class

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-let dictionary = Object.create(null, {
-  toString: { // определяем свойство toString
-    value() { // значение -- это функция
-      return Object.keys(this).join();
-    }
-  }
-});
+function User(name) {
+  
+  this.name = name;
+  this.isAdmin = false;
 
-dictionary.apple = "Apple";
-dictionary.__proto__ = "test";
-
-// apple и __proto__ выведены в цикле
-for(let key in dictionary) {
-  alert(key); // "apple", затем "__proto__"
+  this.sayHi = function() { alert(this.name); };
 }
 
-alert(Object.getOwnPropertyDescriptor(dictionary, 'toString').enumerable); // false
+let user = new User("Вася");
 
-// список свойств, разделённых запятой, выведен с помощью toString
-alert(dictionary); // "apple,__proto__"
+alert(user.name); // Вася
+alert(user.isAdmin); // false
+user.sayHi();
